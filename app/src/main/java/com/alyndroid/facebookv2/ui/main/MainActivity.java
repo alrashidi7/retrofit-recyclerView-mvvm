773283ef9795039509450ec4ff3c608ac24d644f
Alkhiamy Co <mohaalrashidi4@gmail.com>
2019-12-07 07:42:44 +0200

    retrive data by retrofit in recyclerview mvvm

--- app/src/main/java/com/alyndroid/facebookv2/ui/main/MainActivity.java
+++ app/src/main/java/com/alyndroid/facebookv2/ui/main/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alyndroid.facebookv2.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
@@ -10,20 +11,26 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 import com.alyndroid.facebookv2.R;
+import com.alyndroid.facebookv2.databinding.ActivityMainBinding;
 import com.alyndroid.facebookv2.pojo.PostModel;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    ActivityMainBinding binding1;
     PostViewModel postViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //DataBinding
+      binding1 = DataBindingUtil.setContentView(this,R.layout.post_item);
+
+        //ViewModel
         postViewModel = ViewModelProviders.of(this).get(PostViewModel.class);
 
         postViewModel.getPosts();
+        //setupRecyclerView
         RecyclerView recyclerView = findViewById(R.id.recycler);
         final PostsAdapter adapter = new PostsAdapter();
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
--- app/src/main/java/com/alyndroid/facebookv2/ui/main/PostViewModel.java
+++ app/src/main/java/com/alyndroid/facebookv2/ui/main/PostViewModel.java
@@ -1,9 +1,13 @@
 package com.alyndroid.facebookv2.ui.main;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.alyndroid.facebookv2.data.PostsClient;
+import com.alyndroid.facebookv2.R;
+import com.alyndroid.facebookv2.data.PostInterface;
+import com.alyndroid.facebookv2.databinding.ActivityMainBinding;
+import com.alyndroid.facebookv2.databinding.PostItemBinding;
 import com.alyndroid.facebookv2.pojo.PostModel;
 
 import java.util.List;
@@ -11,13 +15,33 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class PostViewModel extends ViewModel {
     MutableLiveData<List<PostModel>> postsMutableLiveData = new MutableLiveData<>();
     MutableLiveData<String> posts = new MutableLiveData<>();
+    private static final String BASE_URL = "http://jsonplaceholder.typicode.com/";
+    private PostInterface postInterface;
+
+
+
+
+
+
 
     public void getPosts() {
-        PostsClient.getINSTANCE().getPosts().enqueue(new Callback<List<PostModel>>() {
+
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        postInterface = retrofit.create(PostInterface.class);
+
+        Call<List<PostModel>> call = postInterface.getPosts();
+
+        call.enqueue(new Callback<List<PostModel>>() {
             @Override
             public void onResponse(Call<List<PostModel>> call, Response<List<PostModel>> response) {
                 postsMutableLiveData.setValue(response.body());
@@ -28,5 +52,12 @@ public class PostViewModel extends ViewModel {
                 posts.setValue("errr");
             }
         });
+
+
+
+
+
+
+
     }
 }
